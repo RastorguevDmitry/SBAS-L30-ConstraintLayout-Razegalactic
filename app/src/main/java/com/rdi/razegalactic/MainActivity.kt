8dbd05ab9ff495da7f66548c659f3920811dc617
rdi81 <rdi81@yandex.ru>
2019-12-04 22:07:55 +0300

    final

--- app/src/main/java/com/rdi/razegalactic/MainActivity.kt
+++ app/src/main/java/com/rdi/razegalactic/MainActivity.kt
@@ -30,21 +30,79 @@
 
 package com.rdi.razegalactic
 
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.transition.AutoTransition
+import android.transition.TransitionManager
+import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import kotlinx.android.synthetic.main.keyframe1.*
 
 /**
  * Main Screen
  */
 class MainActivity : AppCompatActivity() {
 
+    private val constraintSet1 = ConstraintSet()
+    private val constraintSet2 = ConstraintSet()
+
+    private var isOffscreen = true
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+      setContentView(R.layout.keyframe1)
+
+
+
+      constraintSet1.clone(constraintLayout)
+      constraintSet2.clone(this, R.layout.activity_main)
+
+      departButton.setOnClickListener {
+          //1
+          val layoutParams = rocketIcon.layoutParams as ConstraintLayout.LayoutParams
+          val startAngle = layoutParams.circleAngle
+          val endAngle = startAngle + (if (switch1.isChecked) 360 else 180)
+
+
+          val anim = ValueAnimator.ofFloat(startAngle, endAngle)
+          anim.addUpdateListener { valueAnimator ->
+
+
+              val animatedValue = valueAnimator.animatedValue as Float
+              val layoutParams = rocketIcon.layoutParams as ConstraintLayout.LayoutParams
+              layoutParams.circleAngle = animatedValue
+              rocketIcon.layoutParams = layoutParams
+
+
+              rocketIcon.rotation = (animatedValue % 360 - 270)
+          }
+
+          anim.duration = if (switch1.isChecked) 2000 else 1000
+
+
+          anim.interpolator = LinearInterpolator()
+          anim.start()
+      }
+
 
     switch1.setOnCheckedChangeListener { _, isChecked ->
       switch1.setText(if (isChecked) R.string.round_trip else R.string.one_way)
     }
   }
+
+    override fun onEnterAnimationComplete() {
+        super.onEnterAnimationComplete()
+
+        constraintSet2.clone(this, R.layout.activity_main)
+
+        //apply the transition
+        val transition = AutoTransition()
+        transition.duration = 1000
+        TransitionManager.beginDelayedTransition(constraintLayout, transition)
+
+        constraintSet2.applyTo(constraintLayout)
+    }
+
 }
